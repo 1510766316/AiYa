31b0d292ffa52d77fd5057a389b042054fe360ae
guangxi.wu <guangxi.wu@imotorinfo.com>
2016-11-22 18:37:11 +0800

    dd

--- app/src/main/java/app/wgx/com/aiYa/activities/BaseActivity.java
+++ app/src/main/java/app/wgx/com/aiYa/activities/BaseActivity.java
@@ -3,10 +3,14 @@ package app.wgx.com.aiYa.activities;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
 
 import app.wgx.com.aiYa.MyApplication;
 import app.wgx.com.aiYa.service.CheckAppService;
@@ -27,6 +31,15 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         MyApplication.mCurrentContext = this;
+        //状态栏透明
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
         loadLayout(savedInstanceState);
         ButterKnife.bind(this);
         initView();
