514bfec3ec5e595b2f4525dfb1e55d0247f61c01
guangxi.wu <guangxi.wu@imotorinfo.com>
2016-11-23 18:21:53 +0800

    添加侧滑

--- app/src/main/java/app/wgx/com/aiYa/activities/BaseActivity.java
+++ app/src/main/java/app/wgx/com/aiYa/activities/BaseActivity.java
@@ -31,15 +31,6 @@ public abstract class BaseActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         MyApplication.mCurrentContext = this;
-        //状态栏透明
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        }
         loadLayout(savedInstanceState);
         ButterKnife.bind(this);
         initView();
