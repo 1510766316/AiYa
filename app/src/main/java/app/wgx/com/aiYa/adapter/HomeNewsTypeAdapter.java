2eb52783bf5cd8b2249620eb37133e441924ea6e
1510766316 <1510766316@qq.com>
2016-12-02 00:00:11 +0800

    bluetooth

--- app/src/main/java/app/wgx/com/aiYa/adapter/HomeNewsTypeAdapter.java
+++ app/src/main/java/app/wgx/com/aiYa/adapter/HomeNewsTypeAdapter.java
@@ -8,13 +8,6 @@ import java.util.List;
 
 public class HomeNewsTypeAdapter extends BaseQuickAdapter<BaseViewHolder> {
 
-    /**
-     * Same as QuickAdapter#QuickAdapter(Context,int) but with
-     * some initialization data.
-     *
-     * @param layoutResId The layout resource id of each item.
-     * @param data        A new list is created out of this one to avoid mutable list
-     */
     public HomeNewsTypeAdapter(int layoutResId, List<BaseViewHolder> data) {
         super(layoutResId, data);
     }
