bfdecd54fca25baabf7ac39f980dc561eb201d36
guangxi.wu <guangxi.wu@imotorinfo.com>
2017-06-13 15:02:17 +0800

    update libs

--- app/src/main/java/app/wgx/com/aiYa/MyApplication.java
+++ app/src/main/java/app/wgx/com/aiYa/MyApplication.java
@@ -1,18 +1,24 @@
 package app.wgx.com.aiYa;
 
 import android.app.Application;
-import android.content.Context;
 
 import com.lzy.okgo.OkGo;
+import com.lzy.okgo.cache.CacheEntity;
+import com.lzy.okgo.cache.CacheMode;
+import com.lzy.okgo.cookie.CookieJarImpl;
+import com.lzy.okgo.cookie.store.DBCookieStore;
+import com.lzy.okgo.interceptor.HttpLoggingInterceptor;
+import com.lzy.okgo.model.HttpHeaders;
+import com.lzy.okgo.model.HttpParams;
 import com.wenming.library.LogReport;
 import com.wenming.library.save.imp.CrashWriter;
 import com.wenming.library.upload.http.HttpReporter;
 
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
 import app.wgx.com.aiYa.assistTool.VariableTool;
-import app.wgx.com.aiYa.bean.NetworkStateInfo;
-
+import okhttp3.OkHttpClient;
 /**
  * Create view by wgx
  *
@@ -21,35 +27,54 @@ import app.wgx.com.aiYa.bean.NetworkStateInfo;
  * Create at 2016/8/16 11:34
  */
 public class MyApplication extends Application {
-    public static Context mContext, mCurrentContext;
-    public static NetworkStateInfo mNetworkStateInfo;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        mContext = getApplicationContext();
-        mNetworkStateInfo =new NetworkStateInfo();
-        initHttp();
+        initOkGo();
     }
 
 
     /**
      * 初始化网络框架
      */
-    private void initHttp() {
-        OkGo.init(this);
-        try {
-            //以下都不是必须的，根据需要自行选择,一般来说只需要 debug,缓存相关,cookie相关的 就可以了
-            OkGo.getInstance()
-                    // 打开该调试开关,打印级别INFO,并不是异常,是为了显眼,不需要就不要加入该行
-                    // 最后的true表示是否打印okgo的内部异常，一般打开方便调试错误
-                    .debug("OkGo", Level.INFO, true)
-                    //可以全局统一设置超时重连次数,默认为三次,那么最差的情况会请求4次(一次原始请求,三次重连请求),不需要可以设置为0
-                    .setRetryCount(3);
+    private void initOkGo() {
+        //---------这里给出的是示例代码,告诉你可以这么传,实际使用的时候,根据需要传,不需要就不传-------------//
+        HttpHeaders headers = new HttpHeaders();
+        headers.put("commonHeaderKey1", "commonHeaderValue1");    //header不支持中文，不允许有特殊字符
+        headers.put("commonHeaderKey2", "commonHeaderValue2");
+        HttpParams params = new HttpParams();
+        params.put("commonParamsKey1", "commonParamsValue1");     //param支持中文,直接传,不要自己编码
+        params.put("commonParamsKey2", "这里支持中文参数");
+        //----------------------------------------------------------------------------------------//
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        //log相关
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor("OkGo");
+        loggingInterceptor.setPrintLevel(HttpLoggingInterceptor.Level.BODY);        //log打印级别，决定了log显示的详细程度
+        loggingInterceptor.setColorLevel(Level.INFO);                               //log颜色级别，决定了log在控制台显示的颜色
+        builder.addInterceptor(loggingInterceptor);
+        //builder.addInterceptor(new ChuckInterceptor(this));                       //第三方的开源库，使用通知显示当前请求的log
+
+        //超时时间设置，默认60秒
+        builder.readTimeout(OkGo.DEFAULT_MILLISECONDS, TimeUnit.MILLISECONDS);      //全局的读取超时时间
+        builder.writeTimeout(OkGo.DEFAULT_MILLISECONDS, TimeUnit.MILLISECONDS);     //全局的写入超时时间
+        builder.connectTimeout(OkGo.DEFAULT_MILLISECONDS, TimeUnit.MILLISECONDS);   //全局的连接超时时间
+
+        //自动管理cookie（或者叫session的保持），以下几种任选其一就行
+        //builder.cookieJar(new CookieJarImpl(new SPCookieStore(this)));            //使用sp保持cookie，如果cookie不过期，则一直有效
+        builder.cookieJar(new CookieJarImpl(new DBCookieStore(this)));              //使用数据库保持cookie，如果cookie不过期，则一直有效
+        //builder.cookieJar(new CookieJarImpl(new MemoryCookieStore()));            //使用内存保持cookie，app退出后，cookie消失
+
+        // 其他统一的配置
+        // 详细说明看GitHub文档：https://github.com/jeasonlzy/
+        OkGo.getInstance().init(this)                           //必须调用初始化
+                .setOkHttpClient(builder.build())               //设置OkHttpClient
+                .setCacheMode(CacheMode.NO_CACHE)               //全局统一缓存模式，默认不使用缓存，可以不传
+                .setCacheTime(CacheEntity.CACHE_NEVER_EXPIRE)   //全局统一缓存时间，默认永不过期，可以不传
+                .setRetryCount(3)                               //全局统一超时重连次数，默认为三次，那么最差的情况会请求4次(一次原始请求，三次重连请求)，不需要可以设置为0
+                .addCommonHeaders(headers)                      //全局公共头
+                .addCommonParams(params);                       //全局公共参数
     }
 
     private void initCrashReport() {
@@ -75,4 +100,5 @@ public class MyApplication extends Application {
         http.setBodyParam("message");//内容
         LogReport.getInstance().setUploadType(http);
     }
+
 }
